6c5d0de2a1c30011295aee4b399191f8b43619d4
jjjyogurt <ouyangxj3@gmail.com>
2026-01-10 15:34:54 +0800

    i want to just pass the view camera - posture camera image to mediapipe; and let classifer and visual overlay use the same landmarks

--- android-app/app/build/kotlin/compileDebugKotlin/cacheable/caches-jvm/lookups/lookups.tab.values.s
+++ android-app/app/build/kotlin/compileDebugKotlin/cacheable/caches-jvm/lookups/lookups.tab.values.s
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be70dadebfc244dd5e0b606dcc8d75e62560142b66abdbd544a1d61d741e28f2
+oid sha256:1336ac5ff2be1898e126a8d165f3e20249116bfadcd414ab762128b9f35c3e54
 size 2
